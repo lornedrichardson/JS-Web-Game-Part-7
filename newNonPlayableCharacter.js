7d9fceec5a03f1148b8d969da0a995e6ce77fbaf
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-05-11 11:01:56 -0700

    Update newNonPlayableCharacter.js

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -71,13 +71,3 @@ function sleep(time){
         setTimeout(resolve, time)
     })  
 }
-
-async function moveNPC(){
-    await npc.walkNorth(1400)
-    await npc.walkEast(1200)
-    await npc.walkSouth(300)
-    await npc.walkEast(1500)
-    await npc.walkSouth(1500)
-    await npc.walkWest(2700)
-    await npc.walkNorth(400)
-}
