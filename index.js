4a4864dc9d43103524648f1b57d66c82cad9a428
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-05-11 11:03:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const pc = newPlayableCharacter(100, 110)
 // Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
 
-// have the NPC start walking east immediately
-npc.moveNPC()
+// Move the non-playable character on a predefined track
+async function moveNPC(){
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)
+}
+moveNPC()
 
 // Create the inventory
 const inventory = newInventory()
